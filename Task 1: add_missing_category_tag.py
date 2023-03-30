31ddcfa284e69dcf056364963b8507c33ece19ea
Zachary <118567741+zacharyb02@users.noreply.github.com>
2023-03-30 13:23:27 +0000

    Update Task 1: add_missing_category_tag.py

--- Task 1: add_missing_category_tag.py
+++ Task 1: add_missing_category_tag.py
@@ -1,43 +1,35 @@
 import pywikibot
-from copy import deepcopy #to make deep copies instead of shallow copies
+from copy import deepcopy
 
-#!!!!!!!!!!MUST BE FILLED!!!!!!!!!
-NO_CAT_TAG = "{{Amnni_ur_iṭṭafn_taggayt}}"
-SAVE_MESSAGE = "Tamrniwt n tlɣa n imnnitn mi ixwa udɣar n tggayin"
+NO_CAT_TAG = "{{}}"
+SAVE_MESSAGE = ""
 
-#use the default site defined in user-config.py
 site = pywikibot.Site()
 
-ARTICLE_NAMESPACE = 0 #articles
+ARTICLE_NAMESPACE = 0 
 
-#get all pages in the chosen namespace
 pool = site.allpages(namespace=ARTICLE_NAMESPACE)
 
-pool_size = len(list(deepcopy(pool))) #number of articles
+pool_size = len(list(deepcopy(pool)))
 print('Number of articles: '+str(pool_size))
 
 i = 1
 
 for page in pool:
-    #keep track of processed pages
     print('*********'+str(i)+'/'+str(pool_size))
 
-    #number of categories the page has
     cat_count = len(list(page.categories()))
 
-    temp_text = page.text #temporary variable to store the page text
+    temp_text = page.text
 
-    if cat_count == 0: #page has no categories
-        if NO_CAT_TAG not in temp_text: #page does not have the NO_CAT_TAG
-            #adding NO_CAT_TAG to the text at the top
+    if cat_count == 0:
+        if NO_CAT_TAG not in temp_text:
             if "REDIRECT" not in temp_text:
                 temp_text = NO_CAT_TAG + '\n' + temp_text
                 
 
-    if temp_text != page.text: #if the text of the page has been changed by removing or adding the tag
-        #update the text of the page
+    if temp_text != page.text:
         page.text = temp_text
-        #save to wiki
         page.save(SAVE_MESSAGE)
         
     i=i+1
