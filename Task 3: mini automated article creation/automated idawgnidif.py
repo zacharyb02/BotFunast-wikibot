31bf16f25ff60f10057961f1fad043774b4e59e0
Zachary <118567741+zacharyb02@users.noreply.github.com>
2023-04-03 02:39:29 +0000

    Create automated idawgnidif.py

---
+++ Task 3: mini automated article creation/automated idawgnidif.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import pywikibot
+
+data=pd.read_csv("Idawgnidif Data.csv",delimiter=";")
+data3=data[["nb_hab","nb_fam","shi_name","ar_name","taqbilt","shi_taqbilt","tagrawt","shi_tagrawt","nb_hab_2014","nb_fam_2014"]]
+data3
+
+SAVE_MESSAGE='Asnflul n tasna n uḍwwaṛ'
+
+site = pywikibot.Site()
+
+f = open("article 1.txt", "r", encoding="utf8")
+text1=str(f.read())
+f = open("article 2 2014.txt", "r", encoding="utf8")
+text2=str(f.read())
+f = open("article 3.txt", "r", encoding="utf8")
+text3=str(f.read())
+
+for i in range(0,data3.shape[0]):
+    print('*********'+str(i+1)+'/'+str(data3.shape[0])+'************* : '+str(data3['shi_name'][i]))
+    name_final=data3['shi_name'][i]+' ('+data3['shi_taqbilt'][i]+')'
+    page=pywikibot.Page(site,name_final)
+    temp_text = page.text
+    
+    if temp_text=='':
+    
+        ar_name_final = data3['ar_name'][i]+' ('+data3['taqbilt'][i]+')'
+        text_adwwar=text1.replace('shi_name',data3['shi_name'][i]).replace('ar_name',ar_name_final).replace('nb_fam',str(data3['nb_fam'][i])).replace('nb_hab',str(data3['nb_hab'][i])).replace('shi_taqbilt',data3['shi_taqbilt'][i]).replace('shi_tagrawt',data3['shi_tagrawt'][i])
+        temp_text = text_adwwar
+                
+        if np.isnan(data3['nb_hab_2014'][i])==False:
+            text_2014=text2.replace('nb_hab_2014',str(int(data3['nb_hab_2014'][i]))).replace('nb_fam_2014',str(int(data3['nb_fam_2014'][i])))
+            temp_text = temp_text + text_2014
+        
+        temp_text = temp_text + text3.replace('shi_taqbilt',data3['shi_taqbilt'][i]).replace('shi_tagrawt',data3['shi_tagrawt'][i])
+        
+        if temp_text != page.text:
+            page.text = temp_text
+            page.save(SAVE_MESSAGE)
