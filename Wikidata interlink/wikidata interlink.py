851b617319057c20ae92e69ba83e0bfa1a96db9b
Zachary <118567741+zacharyb02@users.noreply.github.com>
2023-04-04 21:55:15 +0000

    Update wikidata interlink.py

--- Wikidata interlink/wikidata interlink.py
+++ Wikidata interlink/wikidata interlink.py
@@ -5,22 +5,22 @@ import pandas as pd
 data=pd.read_csv("Data_Awgnz_Idawgnidif_Wikidata.csv",delimiter=";")
 data2=data[["ar_name","shi_name","taqbilt","shi_taqbilt"]]
 
-SAVE_MESSAGE=""
+SAVE_MESSAGE="Adding shi article"
+
+site1 = pywikibot.Site("ar", "wikipedia")
+site2 = pywikibot.Site("shi", "wikipedia")
 
 site = pywikibot.Site("wikidata", "wikidata")
 
 For i in range(0,data2.shape[0]):
+    print('*********'+str(i+1)+'/'+str(data2.shape[0])+'************* : '+str(data3['shi_name'][i]))
     ar_article_name=str(data2["ar_name"][i]+" ("+data2["taqbilt"][i]+")")
     shi_article_name=str(data2["shi_name"][i]+" ("+data2["shi_taqbilt"][i]+")")
-    source_wiki = pywikibot.Site("ar", "wikipedia")
-    source_page = pywikibot.Page(source_wiki, ar_article_name)
-
-    target_wiki = pywikibot.Site("shi", "wikipedia")
-    target_page = pywikibot.Page(target_wiki, shi_article_name)
-
-    if target_wiki.code not in source_page.interwiki:
-        source_page.interwiki[target_wiki.code] = target_page.title()
-        source_page.save(SAVE_MESSAGE)
-        print("Interwiki link added to " + shi_article_name)
-    else:
-        print("Interwiki link already exists for " + shi_article_name)
+    
+    repo = site.data_repository()
+    page = pywikibot.Page(site1, ar_article_name)
+    item = pywikibot.ItemPage.fromPage(page)
+    page2 = pywikibot.Page(site2, shi_article_name)
+    item.setSitelink(page2, summary=u+SAVE_MESSAGE)
+    print("Interlink done")
+    
