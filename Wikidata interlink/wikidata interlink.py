0034d280d2f15890003dc07df9c2db79b3eeaff6
Zachary <118567741+zacharyb02@users.noreply.github.com>
2023-04-04 22:41:03 +0000

    Update wikidata interlink.py

--- Wikidata interlink/wikidata interlink.py
+++ Wikidata interlink/wikidata interlink.py
@@ -12,15 +12,16 @@ site2 = pywikibot.Site("shi", "wikipedia")
 
 site = pywikibot.Site("wikidata", "wikidata")
 
-For i in range(0,data2.shape[0]):
-    print('*********'+str(i+1)+'/'+str(data2.shape[0])+'************* : '+str(data3['shi_name'][i]))
-    ar_article_name=str(data2["ar_name"][i]+" ("+data2["taqbilt"][i]+")")
-    shi_article_name=str(data2["shi_name"][i]+" ("+data2["shi_taqbilt"][i]+")")
-    
-    repo = site.data_repository()
-    page = pywikibot.Page(site1, ar_article_name)
-    item = pywikibot.ItemPage.fromPage(page)
-    page2 = pywikibot.Page(site2, shi_article_name)
-    item.setSitelink(page2, summary=u+SAVE_MESSAGE)
-    print("Interlink done")
+if __name__ == '__main__':
+    for i in range(0,data2.shape[0]):
+        print('*********'+str(i+1)+'/'+str(data2.shape[0])+'************* : '+str(data3['shi_name'][i]))
+        ar_article_name=str(data2["ar_name"][i]+" ("+data2["taqbilt"][i]+")")
+        shi_article_name=str(data2["shi_name"][i]+" ("+data2["shi_taqbilt"][i]+")")
+
+        repo = site.data_repository()
+        page = pywikibot.Page(site1, ar_article_name)
+        item = pywikibot.ItemPage.fromPage(page)
+        page2 = pywikibot.Page(site2, shi_article_name)
+        item.setSitelink(page2, summary=u+SAVE_MESSAGE)
+        print("Interlink done")
     
